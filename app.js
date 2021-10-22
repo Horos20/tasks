21110495d68e225858f4d4611f72dbba93f951bd
Matis Rammi <matis.rammi@khk.ee>
2021-10-22 09:43:29 +0300

    Added appendChild function to add task project.

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
+const taskInput = document.querySelector("#task");
+const form = document.querySelector("form");
+
+function addTask(event) {
+    // Input value
+    const task = taskInput.value;
+    // Create li element
+    const li = document.createElement("li");
+    // Define <li> CSS class
+    li.className = "collection-item";
+    // Create value for <li>
+    const text = document.createTextNode(task);
+    // Add text value for <li>
+    li.appendChild(text);
+    // Find <ul> DOM component
+    const ul = document.querySelector(".collection");
+    // Add <li> to <ul>
+    ul.appendChild(li);
+    // Clear input value
+    taskInput.value = "";
+    // Form submit event control
+    event.preventDefault();
+}
+
+form.addEventListener("submit" , addTask);
 
